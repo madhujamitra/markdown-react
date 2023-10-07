3a669e54529c2a68a64b8146a78324c250927191
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2023-10-06 17:58:10 -0700

    2nd commit changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React, {useState} from 'react';
-import ReactMarkdown from "react-markdown"
+import React, { useState } from 'react';
+import ReactMarkdown from "react-markdown";
 import './App.css';
-
-
+import { marked } from 'marked';
 
 function App() {
-  const [markdown, setMarkdown] = useState("# madhujamadhuja");
+  const defaultMarkdown = `
+# This is a Header (H1)
+
+## This is a sub-header (H2)
+
+[This is a link](https://www.example.com)
+
+...
+Here's some \`inline code\`.
+...
+\`\`\`
+This is a block of code.
+function hello() {
+  console.log("Hello, World!");
+}
+\`\`\`
+
+- This is a list item
+- Another list item
+
+> This is a blockquote.
+
+![This is an image](https://via.placeholder.com/150)
+
+**This is bolded text.**
+`;
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+
 
   return (
     <div >
       <h2 className='heading'>MarkDown convertor</h2>
       <section className="App" >
         <div className='editor'>
-          <div class="toolbar"><i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>Editor<i class="fa fa-arrows-alt"></i></div>
-          <textarea className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)}> </textarea>
+          <div className="toolbar">Editor</div>
+          <textarea id="editor" className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)}> </textarea>
         </div>
 
         <div className='output'>
-          <div class="toolbar"><i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>Previewer<i class="fa fa-arrows-alt"></i></div>
-          <article className='result'>
-            <ReactMarkdown>{markdown}</ReactMarkdown>
+          <div className="toolbar">Previewer</div>
+          <article className='result' >
+            <div
+              id="preview"
+              dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+            />
           </article>
         </div>
 
       </section>
-
-
     </div>
   );
 }
